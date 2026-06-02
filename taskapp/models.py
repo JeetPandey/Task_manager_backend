63d63bd7fcf9e6b236cd257c25daaa6ade84748b
JeetPandey <dev.jeetpandey25@outlook.com>
2026-06-02 19:54:26 +0530

    some update

--- taskapp/models.py
+++ taskapp/models.py
@@ -33,7 +33,7 @@ class Task(models.Model):
 class Comment(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE,related_name='comments')
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    Comment = models.TextField()
+    comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- taskapp/urls.py
+++ taskapp/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path("tasks/",TaskListCreateAPIView.as_view(),name='task-list'),
-    path("tasks/<int:pk>",TaskDetailAPIView.as_view(),name='task-detail'),
+    path("tasks/<int:pk>/",TaskDetailAPIView.as_view(),name='task-detail'),
     path("login/",TokenObtainPairView.as_view(),name='login'),
     path("refresh/",TokenRefreshView.as_view(),name='token-refresh'),
     #A temp test url for test view
